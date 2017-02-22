78ec88a8fa980fa25f4a5d3c5214395999ade6b6
satyavemas <satyavema@outlook.com>
2017-02-22 12:10:57 +0530

    added regarding inheritance concepts
    we added templated one component inside another component.

---
+++ src/app/app.adminhome.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+
+    selector :'admin-home',
+    templateUrl:'./app.adminhome.html',
+})
+
+export class AppAdminHomeComponent{
+
+    myAdminHome=" Good Admin And Harish is very nice person";
+}
---
+++ src/app/app.myhome.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector :'my-home',
+    templateUrl :'./app.myhome.html',
+})
+export class AppMyHomeComponent{
+    myHomeTitle='this is myhome';
+}
+
